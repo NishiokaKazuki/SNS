4f3343b97818b6aa31a7cefed8e94815a1b3ac87
NishiokaKazuki <n.kazuki.hal@gmail.com>
2020-07-15 17:05:09 +0900

    refs#0002 db:table修正

--- db/initdb.d/002-table.sql
+++ db/initdb.d/002-table.sql
@@ -3,6 +3,7 @@ CREATE TABLE app_users
 (
     id bigserial,
     handle text NOT NULL UNIQUE,
+    password text NOT NULL,
     name text NOT NULL,
     birthday date,
     profile text,
@@ -19,8 +20,9 @@ BEFORE UPDATE ON app_users
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
-CREATE TABLE follow
+CREATE TABLE to_follows
 (
+    id      bigserial,
     to_user bigserial NOT NULL,
     by_user bigserial NOT NULL,
     permit  smallint NOT NULL default 0,
@@ -32,12 +34,12 @@ CREATE TABLE follow
     REFERENCES app_users(id),
 );
 
-CREATE TRIGGER set_update_follow
-BEFORE UPDATE ON follow
+CREATE TRIGGER set_update_to_follows
+BEFORE UPDATE ON to_follows
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
-CREATE TABLE post
+CREATE TABLE posts
 (
     id bigserial,
     user_id bigserial NOT NULL,
@@ -49,17 +51,18 @@ CREATE TABLE post
     FOREIGN KEY (user_id)
     REFERENCES app_users(id),
     FOREIGN KEY (to_post)
-    REFERENCES post(id),
+    REFERENCES posts(id),
     PRIMARY KEY (id)
 );
 
-CREATE TRIGGER set_update_post
-BEFORE UPDATE ON post
+CREATE TRIGGER set_update_posts
+BEFORE UPDATE ON posts
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
-CREATE TABLE praise
+CREATE TABLE praises
 (
+    id bigserial,
     user_id bigserial NOT NULL,
     post_id bigserial NOT NULL,
     disabled boolean NOT NULL DEFAULT false,
@@ -69,14 +72,15 @@ CREATE TABLE praise
     REFERENCES app_users(id),
     FOREIGN KEY (post_id)
     REFERENCES post(id),
+    PRIMARY KEY (id)
 );
 
-CREATE TRIGGER set_update_praise
-BEFORE UPDATE ON praise
+CREATE TRIGGER set_update_praises
+BEFORE UPDATE ON praises
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
 
-CREATE TABLE diffusion
+CREATE TABLE diffusions
 (
     user_id bigserial NOT NULL,
     post_id bigserial NOT NULL,
@@ -89,7 +93,108 @@ CREATE TABLE diffusion
     PRIMARY KEY (id)
 );
 
-CREATE TRIGGER set_update_diffusion
-BEFORE UPDATE ON diffusion
+CREATE TRIGGER set_update_diffusions
+BEFORE UPDATE ON diffusions
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TABLE notifications
+(
+    id         bigserial,
+    user_id    bigserial NOT NULL,
+    type       smallint  NOT NULL,
+    status     smallint  NOT NULL DEFAULT 0,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp on update current_timestamp,
+    FOREIGN KEY (user_id)
+    REFERENCES app_users(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TRIGGER set_update_notifications
+BEFORE UPDATE ON notifications
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TABLE notification_to_follows
+(
+    notification_id bigserial NOT NULL,
+    to_follow_id bigserial NOT NULL,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp on update current_timestamp,
+    FOREIGN KEY (notification_id)
+    REFERENCES notifications(id),
+);
+
+CREATE TRIGGER set_update_notification_to_follows
+BEFORE UPDATE ON notification_to_follows
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TABLE notification_praises
+(
+    notification_id bigserial NOT NULL,
+    praise_id bigserial NOT NULL,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp on update current_timestamp,
+    FOREIGN KEY (notification_id)
+    REFERENCES notifications(id),
+    FOREIGN KEY (praise_id)
+    REFERENCES praises(id),
+);
+
+CREATE TRIGGER set_update_notification_to_follows
+BEFORE UPDATE ON notification_follows
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TABLE notification_praises
+(
+    notification_id bigserial NOT NULL,
+    praise_id bigserial NOT NULL,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp on update current_timestamp,
+    FOREIGN KEY (notification_id)
+    REFERENCES notifications(id),
+    FOREIGN KEY (praise_id)
+    REFERENCES praises(id),
+);
+
+CREATE TRIGGER set_update_notification_praises
+BEFORE UPDATE ON notification_praises
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TABLE notification_diffusions
+(
+    notification_id bigserial NOT NULL,
+    diffusion_id bigserial NOT NULL,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp on update current_timestamp,
+    FOREIGN KEY (notification_id)
+    REFERENCES notifications(id),
+    FOREIGN KEY (diffusion_id)
+    REFERENCES diffusions(id),
+);
+
+CREATE TRIGGER set_update_notification_diffusions
+BEFORE UPDATE ON notification_diffusions
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TABLE notification_mentions
+(
+    notification_id bigserial NOT NULL,
+    post_id bigserial NOT NULL,
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp NOT NULL DEFAULT current_timestamp on update current_timestamp,
+    FOREIGN KEY (notification_id)
+    REFERENCES notifications(id),
+    FOREIGN KEY (post_id)
+    REFERENCES posts(id),
+);
+
+CREATE TRIGGER set_update_notification_mentions
+BEFORE UPDATE ON notification_mentions
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
--- db/initdb.d/999-dummy.sql
+++ db/initdb.d/999-dummy.sql
@@ -1,36 +1,37 @@
 \c sns
 
-INSERT INTO app_uses(id, handle, name, birthday, profile, image, is_private) VALUES
-    (1, "test_01", "テスト1", "2000-01-01", "こんにちは", "", false),
-    (2, "test_02", "テスト2", "2000-01-02", "やあ", "", false),
-    (3, "test_03", "テスト3", "2000-01-03", "３", "", false),
-    (4, "test_04", "テスト4", "2000-01-04", "４", "", false),
-    (5, "test_05", "テスト5", "2000-01-05", "５", "", false),
-    (6, "test_06", "テスト6", "2000-01-06", "６", "", true);
+INSERT INTO app_users(id, handle, password, name, birthday, profile, image, is_private) VALUES
+    (1, "test_01", "secret", "テスト1", "2000-01-01", "こんにちは", "", false),
+    (2, "test_02", "secret", "テスト2", "2000-01-02", "やあ", "", false),
+    (3, "test_03", "secret", "テスト3", "2000-01-03", "３", "", false),
+    (4, "test_04", "secret", "テスト4", "2000-01-04", "４", "", false),
+    (5, "test_05", "secret", "テスト5", "2000-01-05", "５", "", false),
+    (6, "test_06", "secret", "テスト6", "2000-01-06", "６", "", true);
 
 /* 0;未確認 1:許可 2:拒否 */
-INSERT INTO follow(to_user, by_user, permit) VALUES
-    (1, 2, 1 ),
-    (1, 3, 1 ),
-    (1, 4, 1 ),
-    (1, 5, 1 ),
-    (1, 6, 1 ),
-    (2, 1, 1 ),
-    (2, 5, 1 ),
-    (3, 2, 1 ),
-    (4, 6, 0 ),
-    (5, 6, 2 );
+INSERT INTO to_follows(id, to_user, by_user, permit) VALUES
+    (1, 1, 2, 1 ),
+    (2, 1, 3, 1 ),
+    (3, 1, 4, 1 ),
+    (4, 1, 5, 1 ),
+    (5, 1, 6, 1 ),
+    (6, 2, 1, 1 ),
+    (7, 2, 5, 1 ),
+    (8, 3, 2, 1 ),
+    (9, 6, 4, 0 ),
+    (10, 6, 3, 1 ),
+    (11, 6, 5, 2 );
 
-INSERT INTO post(id, user_id, to_post, body) VALUES
+INSERT INTO posts(id, user_id, to_post, body) VALUES
     (1, 1, 0, "投稿1"),
     (2, 2, 0, "投稿2"),
     (3, 3, 0, "投稿3"),
     (4, 4, 0, "投稿4"),
     (5, 5, 0, "投稿5"),
     (6, 6, 0, "投稿6"),
-    (7, 1, 2, "投稿2へ送信");
+    (7, 1, 2, "@test_02 投稿2へ送信");
 
-INSERT INTO praise(user_id, post_id) VALUES
+INSERT INTO praises(user_id, post_id) VALUES
     (1, 2),
     (1, 3),
     (1, 4),
@@ -42,10 +43,36 @@ INSERT INTO praise(user_id, post_id) VALUES
     (6, 1),
     (3, 7);
 
-INSERT INTO diffusion(user_id, post_id) VALUES
+INSERT INTO diffusions(user_id, post_id) VALUES
     (1, 2),
     (1, 3),
     (1, 4),
     (1, 5),
     (3, 1),
     (6, 1);
+
+/* type   0:to_follows 1:praises 2:diffusions 3:mentions*/
+/* status 0:未確認 1:確認済み 2:その他 */
+INSERT INTO notifications(id, user_id, type, status) VALUES
+    (1, 1, 0, 0),
+    (2, 1, 1, 0),
+    (3, 2, 2, 0),
+    (4, 1, 3, 0),
+    (5, 1, 0, 1),
+    (6, 6, 0, 0),
+    (7, 6, 0, 1);
+
+INSERT INTO notification_to_follows(notification_id, to_follow_id) VALUES
+    (1, 1),
+    (5, 2),
+    (6, 9),
+    (7, 10);
+
+INSERT INTO notification_to_praises(notification_id, praise_id) VALUES
+    (2, 1);
+
+INSERT INTO notification_diffusions(notification_id, diffusion_id) VALUES
+    (3, 1);
+
+INSERT INTO notification_mentions(notification_id, post_id) VALUES
+    (4, 7);
