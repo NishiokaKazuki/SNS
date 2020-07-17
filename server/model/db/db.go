1bb073e212cf05ce5d207af770263d2e743b640e
NishiokaKazuki <n.kazuki.hal@gmail.com>
2020-07-17 13:54:25 +0900

    refs#1002 api:db接続設定

---
+++ server/model/db/db.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"server/config"
+
+	"github.com/go-xorm/xorm"
+)
+
+const (
+	configpath = "configs/config.toml"
+)
+
+var engine = xormConnect()
+
+func GetDBConnect() *xorm.xEngine {
+	return engine
+}
+
+func xormConnect() *xorm.Engine {
+
+	db, err := xorm.NewEngine(getDBConfig())
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
+
+func getDBConfig() (string, string) {
+	conf, err := config.ReadDBConfig(configpath)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	CONNECT := conf.User + ":" + conf.Pass + "@" + conf.Protocol + "/" + conf.Dbname + "?parseTime=true"
+
+	return conf.Dbms, CONNECT
+}
