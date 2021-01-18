545816b8dfec13486c8b8941a908243c433e1597
NishiokaKazuki <n.kazuki.hal@gmail.com>
2021-01-18 18:07:30 +0900

    refs#2003 Add signin button on header

--- client/src/containers/Header/index.tsx
+++ client/src/containers/Header/index.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar'
 import GitHubIcon from '@material-ui/icons/GitHub'
 
 const Header: React.FC = () => {
+    const classes = useStyles()
     return (
         <>
             <div>
@@ -15,6 +18,11 @@ const Header: React.FC = () => {
                         <StyledLink to="/">
                             <GitHubIcon />チャット
                         </StyledLink>
+                        <ButtonLink to="signin">
+                            <Button className={classes.button}>
+                                SignIn
+                            </Button>
+                        </ButtonLink>
                     </Toolbar>
                 </AppBar>
             </div>
@@ -22,10 +30,21 @@ const Header: React.FC = () => {
     )
 }
 
+const useStyles = makeStyles({
+    button: {
+      color: 'white',
+    },
+});
+
+
 const StyledLink = styled(Link)`
     margin: auto;
     text-decoration: none;
     color: white;
+`
+
+const ButtonLink = styled(Link)`
+    text-decoration: none;
 `
 
 export default Header
